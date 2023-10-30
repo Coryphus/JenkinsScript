ab289b708a23a8ba1af6ca52da4689adc1d31bd7
Coryphus <103670797+Coryphus@users.noreply.github.com>
2023-10-30 11:32:22 +0000

    Better script potentially

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -5,13 +5,11 @@ sudo apt update && sudo apt upgrade -y
 sudo apt-get install -y openjdk-11-jre
 
 
-sudo bash
-
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key \
+sudo curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key \
         | sudo tee /usr/share/keyrings/jenkins-keyring.asc > \
         /dev/null
 
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
         https://pkg.jenkins.io/debian binary/ | \
         sudo tee /etc/apt/sources.list.d/jenkins.list > \
         /dev/null
